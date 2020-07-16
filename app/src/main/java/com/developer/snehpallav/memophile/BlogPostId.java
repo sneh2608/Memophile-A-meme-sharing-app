915e3e91f6fbc005e82c14913070f5a6d5647060
Sneh Pallav <snehpallav97@gmail.com>
2020-07-16 16:29:16 +0530

    memophile  Initial commit

---
+++ app/src/main/java/com/developer/snehpallav/memophile/BlogPostId.java
@@ -0,0 +1,19 @@
+package com.developer.snehpallav.memophile;
+
+
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.firestore.Exclude;
+
+public class BlogPostId {
+
+    @Exclude
+    public String BlogPostId;
+
+    public <T extends BlogPostId> T withId(@NonNull final String id) {
+        this.BlogPostId = id;
+        return (T) this;
+    }
+
+}
